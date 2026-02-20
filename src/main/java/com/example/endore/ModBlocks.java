a8d97f99f2661700274d7def03384802a6b6eaca
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-20 21:31:21 +0800

    Update ModBlocks.java

--- src/main/java/com/example/endore/ModBlocks.java
+++ src/main/java/com/example/endore/ModBlocks.java
@@ -1,7 +1,6 @@
 package com.example.endore;
 
 import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModBlocks {
@@ -10,10 +9,7 @@ public class ModBlocks {
 
     public static void init() {
 
-        BROKEN_ORE = new BlockBrokenOre()
-                .setRegistryName("broken_ore")
-                .setUnlocalizedName("broken_ore")
-                .setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        BROKEN_ORE = new BlockBrokenOre();
 
         ForgeRegistries.BLOCKS.register(BROKEN_ORE);
     }
