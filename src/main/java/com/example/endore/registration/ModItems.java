7624c29fc3b700d66533c38aecf6af7d459d0bd7
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-20 22:15:19 +0800

    Update ModItems.java

--- src/main/java/com/example/endore/registration/ModItems.java
+++ src/main/java/com/example/endore/registration/ModItems.java
@@ -1,7 +1,6 @@
 package com.example.endore.registration;
 
 import com.example.endore.EndOreMod;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
