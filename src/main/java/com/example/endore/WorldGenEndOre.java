d75179549fdeae21b15db5601ebb8ec39c79b182
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-20 21:29:19 +0800

    Update WorldGenEndOre.java

--- src/main/java/com/example/endore/WorldGenEndOre.java
+++ src/main/java/com/example/endore/WorldGenEndOre.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProviderEnd;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
@@ -18,20 +19,19 @@ public class WorldGenEndOre implements IWorldGenerator {
         generator = new WorldGenMinable(
                 ModBlocks.BROKEN_ORE.getDefaultState(),
                 ConfigHandler.veinSize,
-                block -> block.getBlock() == Blocks.END_STONE
+                state -> state.getBlock() == Blocks.END_STONE
         );
     }
 
     @Override
-    public void generate(
-            Random rand,
-            int chunkX,
-            int chunkZ,
-            World world,
-            IChunkProvider chunkGenerator,
-            IChunkProvider chunkProvider
-    ) {
+    public void generate(Random random,
+                         int chunkX,
+                         int chunkZ,
+                         World world,
+                         IChunkGenerator chunkGenerator,
+                         IChunkProvider chunkProvider) {
 
+        // 只在末地
         if (!(world.provider instanceof WorldProviderEnd)) return;
 
         int baseX = chunkX * 16;
@@ -39,11 +39,11 @@ public class WorldGenEndOre implements IWorldGenerator {
 
         for (int i = 0; i < ConfigHandler.spawnTries; i++) {
 
-            int x = baseX + rand.nextInt(16);
-            int y = ConfigHandler.minY + rand.nextInt(ConfigHandler.maxY - ConfigHandler.minY + 1);
-            int z = baseZ + rand.nextInt(16);
+            int x = baseX + random.nextInt(16);
+            int y = ConfigHandler.minY + random.nextInt(ConfigHandler.maxY - ConfigHandler.minY + 1);
+            int z = baseZ + random.nextInt(16);
 
-            generator.generate(world, rand, new BlockPos(x, y, z));
+            generator.generate(world, random, new BlockPos(x, y, z));
         }
     }
 }
