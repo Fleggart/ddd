b4beec0063894e9369cd05d49fb9b9d5df41f1cf
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 03:39:24 +0800

    Update DamagedEndOreBlock.java

--- src/main/java/com/example/endore/blocks/DamagedEndOreBlock.java
+++ src/main/java/com/example/endore/blocks/DamagedEndOreBlock.java
@@ -3,13 +3,11 @@ package com.example.endore.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.Explosion;
-
-import java.util.Random;
 
 public class DamagedEndOreBlock extends Block {
 
@@ -17,37 +15,18 @@ public class DamagedEndOreBlock extends Block {
         super(material);
     }
 
+    // 掉落自身
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return Items.ENDER_PEARL;
+        return Item.getItemFromBlock(this);
     }
 
-    @Override
-    public int quantityDroppedWithBonus(int fortune, Random random) {
-        if (fortune > 0) {
-            int i = random.nextInt(fortune + 2) - 1;
-            if (i < 0) i = 0;
-            return this.quantityDropped(random) * (i + 1);
-        }
-        return this.quantityDropped(random);
-    }
-
-    @Override
-    public int quantityDropped(Random random) {
-        return random.nextBoolean() ? 1 : 0;
-    }
-
-    @Override
-    public int getExpDrop(IBlockState state, net.minecraft.world.IBlockAccess world, BlockPos pos, int fortune) {
-        return 2;
-    }
-
-    // 破损矿石被爆炸破坏时，100%掉落物品
+    // 爆炸时掉落自身
     @Override
     public void onBlockExploded(World world, BlockPos pos, Explosion explosion) {
-        // 先掉落物品
-        this.dropBlockAsItem(world, pos, this.getDefaultState(), 0);
-        // 然后正常移除方块
+        if (!world.isRemote) {
+            spawnAsEntity(world, pos, new ItemStack(this));
+        }
         super.onBlockExploded(world, pos, explosion);
     }
 }
