b981115a3673af81e62bba7378d67144927929a0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-20 21:48:50 +0800

    Update ModBlocks.java

--- src/main/java/com/example/endore/ModBlocks.java
+++ src/main/java/com/example/endore/ModBlocks.java
@@ -1,6 +1,8 @@
 package com.example.endore;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModBlocks {
@@ -8,9 +10,12 @@ public class ModBlocks {
     public static Block BROKEN_ORE;
 
     public static void init() {
-
         BROKEN_ORE = new BlockBrokenOre();
-
         ForgeRegistries.BLOCKS.register(BROKEN_ORE);
+
+        // 注册对应的 ItemBlock
+        ItemBlock itemBlock = new ItemBlock(BROKEN_ORE);
+        itemBlock.setRegistryName(BROKEN_ORE.getRegistryName()); // 使用相同的注册名
+        ForgeRegistries.ITEMS.register(itemBlock);
     }
 }
