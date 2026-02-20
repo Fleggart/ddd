3514a339ffb20e61c6dc9fcb733af09bf49d70e4
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 02:57:33 +0800

    Create DamagedEndOreBlock.java

---
+++ src/main/java/com/example/endore/blocks/DamagedEndOreBlock.java
@@ -0,0 +1,49 @@
+package com.example.endore.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class DamagedEndOreBlock extends Block {
+
+    public DamagedEndOreBlock(Material material) {
+        super(material);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Items.ENDER_PEARL;
+    }
+
+    @Override
+    public int quantityDroppedWithBonus(int fortune, Random random) {
+        if (fortune > 0) {
+            int i = random.nextInt(fortune + 2) - 1;
+            if (i < 0) i = 0;
+            return this.quantityDropped(random) * (i + 1);
+        }
+        return this.quantityDropped(random);
+    }
+
+    @Override
+    public int quantityDropped(Random random) {
+        return random.nextBoolean() ? 1 : 0;
+    }
+
+    @Override
+    public int getExpDrop(IBlockState state, net.minecraft.world.IBlockAccess world, BlockPos pos, int fortune) {
+        return 2;
+    }
+
+    @Override
+    public void onBlockExploded(World world, BlockPos pos, IBlockState state) {
+        this.dropBlockAsItem(world, pos, state, 0);
+        super.onBlockExploded(world, pos, state);
+    }
+}
