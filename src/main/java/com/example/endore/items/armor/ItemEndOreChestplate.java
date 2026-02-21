4eae611e8840e448123949688751926e5a77b415
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:11:50 +0800

    Update ItemEndOreChestplate.java

--- src/main/java/com/example/endore/items/armor/ItemEndOreChestplate.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreChestplate.java
@@ -1,6 +1,7 @@
-
 package com.example.endore.items.armor;
 
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 
@@ -9,8 +10,11 @@ public class ItemEndOreChestplate extends ItemArmor {
     public ItemEndOreChestplate() {
         super(EndOreArmorMaterial.END_ORE, 1, EntityEquipmentSlot.CHEST);
         
-        this.setRegistryName("end_ore_chestplate");
+        this.setRegistryName(EndOreMod.MODID, "end_ore_chestplate");
         this.setTranslationKey(EndOreMod.MODID + ".end_ore_chestplate");
         this.setCreativeTab(CreativeTabs.COMBAT);
+        
+        // 设置击退抗性
+        this.setKnockbackResistance(0.4F);
     }
 }
