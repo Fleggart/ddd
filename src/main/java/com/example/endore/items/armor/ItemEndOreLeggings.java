f6003a9424c041bd06cc9f0462fade1b7209276e
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:04:05 +0800

    Update Gradle properties for memory and LWJGL settings

--- src/main/java/com/example/endore/items/armor/ItemEndOreLeggings.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreLeggings.java
@@ -1,1 +1,16 @@
 
+package com.example.endore.items.armor;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ItemEndOreLeggings extends ItemArmor {
+    
+    public ItemEndOreLeggings() {
+        super(EndOreArmorMaterial.END_ORE, 2, EntityEquipmentSlot.LEGS);
+        
+        this.setRegistryName("end_ore_leggings");
+        this.setTranslationKey(EndOreMod.MODID + ".end_ore_leggings");
+        this.setCreativeTab(CreativeTabs.COMBAT);
+    }
+}
