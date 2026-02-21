237f39f1f3f5935c620025ca47fc3a8519c39190
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-22 06:31:16 +0800

    Update EndOreToolMaterial.java

--- src/main/java/com/example/endore/items/tools/EndOreToolMaterial.java
+++ src/main/java/com/example/endore/items/tools/EndOreToolMaterial.java
@@ -1,16 +1,17 @@
 package com.example.endore.items.tools;
 
 import com.example.endore.EndOreMod;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 
-public class ItemEndOreSword extends ItemSword {
+public class EndOreToolMaterial {
     
-    public ItemEndOreSword() {
-        super(EndOreToolMaterial.END_ORE);
-        
-        this.setRegistryName(EndOreMod.MODID, "end_ore_sword");
-        this.setTranslationKey(EndOreMod.MODID + ".end_ore_sword");
-        this.setCreativeTab(CreativeTabs.COMBAT);
-    }
+    public static final ToolMaterial END_ORE = EnumHelper.addToolMaterial(
+        "END_ORE",           // 名称
+        3,                   // 挖掘等级 (0=木, 1=石, 2=铁, 3=钻石, 4=更高)
+        1800,                // 耐久度
+        8.0F,                // 挖掘速度
+        4.0F,                // 攻击伤害加成
+        22                   // 附魔能力
+    );
 }
