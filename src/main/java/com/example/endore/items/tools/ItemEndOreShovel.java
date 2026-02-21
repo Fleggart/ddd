be2a574adb6b624e6df8cb57ed7a9eb152dfa016
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-22 06:24:35 +0800

    Update ItemEndOreShovel.java

--- src/main/java/com/example/endore/items/tools/ItemEndOreShovel.java
+++ src/main/java/com/example/endore/items/tools/ItemEndOreShovel.java
@@ -1,1 +1,17 @@
 
+package com.example.endore.items.tools;
+
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSpade;
+
+public class ItemEndOreShovel extends ItemSpade {
+    
+    public ItemEndOreShovel() {
+        super(EndOreToolMaterial.END_ORE);
+        
+        this.setRegistryName(EndOreMod.MODID, "end_ore_shovel");
+        this.setTranslationKey(EndOreMod.MODID + ".end_ore_shovel");
+        this.setCreativeTab(CreativeTabs.TOOLS);
+    }
+}
