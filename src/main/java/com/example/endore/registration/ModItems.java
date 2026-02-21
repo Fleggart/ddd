292a1ccbb0d747debf07f05b2d95a590416f70fc
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 12:32:33 +0800

    Update ModItems.java

--- src/main/java/com/example/endore/registration/ModItems.java
+++ src/main/java/com/example/endore/registration/ModItems.java
@@ -2,18 +2,18 @@ package com.example.endore.registration;
 
 import com.example.endore.EndOreMod;
 import com.example.endore.items.armor.*;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = EndOreMod.MODID)
 public class ModItems {
+
+    // 方块物品
+    public static Item END_ORE_ITEM_BLOCK;
+    public static Item DAMAGED_END_ORE_ITEM_BLOCK;
 
     // 盔甲物品
     public static Item END_ORE_HELMET;
@@ -24,11 +24,11 @@ public class ModItems {
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         // 注册方块物品
-        ItemBlock endOreItemBlock = new ItemBlock(ModBlocks.END_ORE_BLOCK);
-        endOreItemBlock.setRegistryName(ModBlocks.END_ORE_BLOCK.getRegistryName());
+        END_ORE_ITEM_BLOCK = new ItemBlock(ModBlocks.END_ORE_BLOCK)
+                .setRegistryName(ModBlocks.END_ORE_BLOCK.getRegistryName());
         
-        ItemBlock damagedEndOreItemBlock = new ItemBlock(ModBlocks.DAMAGED_END_ORE_BLOCK);
-        damagedEndOreItemBlock.setRegistryName(ModBlocks.DAMAGED_END_ORE_BLOCK.getRegistryName());
+        DAMAGED_END_ORE_ITEM_BLOCK = new ItemBlock(ModBlocks.DAMAGED_END_ORE_BLOCK)
+                .setRegistryName(ModBlocks.DAMAGED_END_ORE_BLOCK.getRegistryName());
 
         // 创建盔甲物品
         END_ORE_HELMET = new ItemEndOreHelmet();
@@ -38,8 +38,8 @@ public class ModItems {
 
         // 注册所有物品
         event.getRegistry().registerAll(
-            endOreItemBlock,
-            damagedEndOreItemBlock,
+            END_ORE_ITEM_BLOCK,
+            DAMAGED_END_ORE_ITEM_BLOCK,
             END_ORE_HELMET,
             END_ORE_CHESTPLATE,
             END_ORE_LEGGINGS,
