cdac80d7bc8dc80884cbce71b2411df3b22f87da
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:03:21 +0800

    Update ItemEndOreBoots.java

--- src/main/java/com/example/endore/items/armor/ItemEndOreBoots.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreBoots.java
@@ -1,1 +1,16 @@
 
+package com.example.endore.items.armor;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ItemEndOreBoots extends ItemArmor {
+    
+    public ItemEndOreBoots() {
+        super(EndOreArmorMaterial.END_ORE, 1, EntityEquipmentSlot.FEET);
+        
+        this.setRegistryName("end_ore_boots");
+        this.setTranslationKey(EndOreMod.MODID + ".end_ore_boots");
+        this.setCreativeTab(CreativeTabs.COMBAT);
+    }
+}
