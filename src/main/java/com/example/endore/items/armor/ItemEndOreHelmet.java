9d35e070d3aa8042b752cb4f843e125ba6bae57c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:11:25 +0800

    Update ItemEndOreHelmet.java

--- src/main/java/com/example/endore/items/armor/ItemEndOreHelmet.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreHelmet.java
@@ -1,6 +1,7 @@
-
 package com.example.endore.items.armor;
 
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 
@@ -9,8 +10,11 @@ public class ItemEndOreHelmet extends ItemArmor {
     public ItemEndOreHelmet() {
         super(EndOreArmorMaterial.END_ORE, 1, EntityEquipmentSlot.HEAD);
         
-        this.setRegistryName("end_ore_helmet");
+        this.setRegistryName(EndOreMod.MODID, "end_ore_helmet");
         this.setTranslationKey(EndOreMod.MODID + ".end_ore_helmet");
         this.setCreativeTab(CreativeTabs.COMBAT);
+        
+        // 设置击退抗性
+        this.setKnockbackResistance(0.4F);
     }
 }
