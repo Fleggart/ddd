064ced7a40ad52c24ed355b0a65209d470cdb551
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:12:49 +0800

    Update ItemEndOreLeggings.java

--- src/main/java/com/example/endore/items/armor/ItemEndOreLeggings.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreLeggings.java
@@ -1,6 +1,7 @@
-
 package com.example.endore.items.armor;
 
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 
@@ -9,8 +10,11 @@ public class ItemEndOreLeggings extends ItemArmor {
     public ItemEndOreLeggings() {
         super(EndOreArmorMaterial.END_ORE, 2, EntityEquipmentSlot.LEGS);
         
-        this.setRegistryName("end_ore_leggings");
+        this.setRegistryName(EndOreMod.MODID, "end_ore_leggings");
         this.setTranslationKey(EndOreMod.MODID + ".end_ore_leggings");
         this.setCreativeTab(CreativeTabs.COMBAT);
+        
+        // 设置击退抗性
+        this.setKnockbackResistance(0.4F);
     }
 }
