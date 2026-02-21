2e77ece4571f953b2da3a737453609c04cc99632
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-21 08:13:41 +0800

    Update ItemEndOreBoots.java

--- src/main/java/com/example/endore/items/armor/ItemEndOreBoots.java
+++ src/main/java/com/example/endore/items/armor/ItemEndOreBoots.java
@@ -1,6 +1,7 @@
-
 package com.example.endore.items.armor;
 
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 
@@ -9,8 +10,11 @@ public class ItemEndOreBoots extends ItemArmor {
     public ItemEndOreBoots() {
         super(EndOreArmorMaterial.END_ORE, 1, EntityEquipmentSlot.FEET);
         
-        this.setRegistryName("end_ore_boots");
+        this.setRegistryName(EndOreMod.MODID, "end_ore_boots");
         this.setTranslationKey(EndOreMod.MODID + ".end_ore_boots");
         this.setCreativeTab(CreativeTabs.COMBAT);
+        
+        // 设置击退抗性
+        this.setKnockbackResistance(0.4F);
     }
 }
