a53aa4562fde154de90f954a3bc9cf52fd38ae20
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-22 06:36:06 +0800

    Update ItemEndOreAxe.java

--- src/main/java/com/example/endore/items/tools/ItemEndOreAxe.java
+++ src/main/java/com/example/endore/items/tools/ItemEndOreAxe.java
@@ -1,1 +1,16 @@
+package com.example.endore.items.tools;
 
+import com.example.endore.EndOreMod;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemAxe;
+
+public class ItemEndOreAxe extends ItemAxe {
+    
+    public ItemEndOreAxe() {
+        super(EndOreToolMaterial.END_ORE, 9.0F, -3.0F);
+        
+        this.setRegistryName(EndOreMod.MODID, "end_ore_axe");
+        this.setTranslationKey(EndOreMod.MODID + ".end_ore_axe");
+        this.setCreativeTab(CreativeTabs.TOOLS);
+    }
+}
